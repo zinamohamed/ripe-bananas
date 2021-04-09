eb1f54b15b9f6ed2e034cb92a9f229c7a013680f
Dylan White <dylan.whitej@gmail.com>
2021-04-09 15:31:17 -0700

    first route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,4 +6,23 @@ describe('ripe-bananas routes', () => {
   beforeEach(() => {
     return db.sync({ force: true });
   });
+  const studio = {
+    name: "Zina's Production Haus",
+    city: 'New Orleans',
+    state: 'LA',
+    country: 'United States',
+  }
+  it('should creates a new studio and inserts into database', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send(studio)
+      .then(res => {
+        expect(res.body).toEqual({
+          id: 1,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          ...studio
+        })
+      })
+  })
 });
