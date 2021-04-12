f00c5e98fe262264bda7b61b6d2d4533952a51a1
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-04-12 11:00:52 -0700

    actors post test passing

--- __tests__/actors.test.js
+++ __tests__/actors.test.js
@@ -2,32 +2,34 @@ const request = require('supertest');
 const app = require('../lib/app');
 const db = require('../lib/utils/sequelize');
 
-describe('ripe-bananas routes', () => {
+describe('ripe-bananas actor routes', () => {
     beforeEach(() => {
         return db.sync({ force: true });
     });
-    const actors = [
+    const actor = [
         {
             name: 'Angelina Jolie',
-            date_of_birth: 'June 4, 1975',
-            place_of_birth: 'Los Angeles'
+            dob: '1975-06-04T07:00:00.000Z',
+            pob: 'Los Angeles'
         },
         {
             name: 'Denzel Washington',
-            date_of_birth: 'December 28, 1954',
-            place_of_birth: 'Mount Vernon'
+            dob: expect.any(Date),
+            pob: 'Mount Vernon'
         }
     ];
-    it('should creates a new actor and insert it into the db', () => {
+    it('should create a new actor and insert it into the db', () => {
         return request(app)
             .post('/api/v1/actors')
-            .send(studio[0])
+            .send(actor[0])
             .then((res) => {
                 expect(res.body).toEqual({
                     id: 1,
                     createdAt: expect.any(String),
                     updatedAt: expect.any(String),
-                    ...studio[0],
+                    name: 'Angelina Jolie',
+                    dob: '1975-06-04T07:00:00.000Z',
+                    pob: 'Los Angeles',
                 });
             });
     });
