fa769d826967a04d1c89a1f96e6f97373f3b958b
Dominique Sakrisson <domsakrisson@gmailcom>
2021-04-12 13:38:44 -0700

    Update function works, test

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -45,14 +45,22 @@ describe('ripe-bananas routes', () => {
                 {id:3, createdAt: expect.any(String), updatedAt: expect.any(String), ...reviewers[1]},
             ])
         })
-    })
+    });
     it('gets a particular reviewer by id', () => {
         return request(app)
         .get('/api/v1/reviewers/3')
-        
         .then((res) => {
-            console.log(res.body);
             expect(res.body).toEqual({id: 3, createdAt: expect.any(String), updatedAt: expect.any(String), ...reviewers[1]})
         })
-    })
+    });
+    it('updates a particular reviewer by id', () => {
+        return request(app)
+        .put('/api/v1/reviewers/3')
+        .send({name: 'third reviewer', company: 'review shack'})
+
+        .then((res) => {
+            expect(res.body[1][0]).toEqual({id: 3, name:'third reviewer', company: 'review shack', createdAt: expect.any(String), updatedAt: expect.any(String)})
+        })
+        
+    });
 })
