f6d8c2b9ca686973f6ca309a64c10cbae171832c
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-04-12 13:43:44 -0700

    get ALL films tests written and passing

--- __tests__/films.test.js
+++ __tests__/films.test.js
@@ -32,4 +32,54 @@ describe('ripe-bananas film routes', () => {
               });
           });
     });
+
+    it('test /batch route', () => {
+      return request(app)
+          .post('/api/v1/films/batch')
+          .send(film)
+          .then((res) => {
+              expect(res.body).toEqual([
+                  {
+                      id: 1,
+                      createdAt: expect.any(String),
+                      updatedAt: expect.any(String),
+                      ...film[0],
+                  },
+                  {
+                      id: 2,
+                      createdAt: expect.any(String),
+                      updatedAt: expect.any(String),
+                      ...film[1],
+                  }
+          ]);
+      });
+  });
+  
+    it('should get ALL films', async () => {
+      await request(app)
+          .post('/api/v1/films/batch')
+          .send(film)
+      const response = await request(app)
+          .get('/api/v1/films')
+          const allFilms = 
+          [
+            {
+                id: 1,
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String),
+                title: 'Mean Girls',
+                studio: 5,
+                released: 2004,
+            },
+            {
+                id: 2,
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String),
+                title: 'Eternal Sunshine of the Spotless Mind',
+                studio: 2,
+                released: 2004,
+            }
+        ];
+      expect(response.body).toEqual(allFilms);
+  })
 });
